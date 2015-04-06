93deddb083e2c8eb077a0c75d2bf3b7f6a09a5b3
sam zilverberg <samzilverberg@gmail.com>
2015-04-06 11:23:22 +0200

    refactor switch case to handler fns

--- src/android/MixpanelPlugin.java
+++ src/android/MixpanelPlugin.java
@@ -17,7 +17,6 @@ import org.json.JSONObject;
 public class MixpanelPlugin extends CordovaPlugin {
 
     private static String LOG_TAG = "MIXPANEL PLUGIN";
-
     private static MixpanelAPI mixpanel;
 
     private enum Action {
@@ -32,25 +31,18 @@ public class MixpanelPlugin extends CordovaPlugin {
         private static final Map<String, Action> lookup = new HashMap<String, Action>();
 
         static {
-            for (Action a : Action.values())
-                lookup.put(a.getName(), a);
+            for (Action a : Action.values()) lookup.put(a.getName(), a);
         }
 
-        private Action(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public static Action get(String name) {
-            return lookup.get(name);
-        }
-
+        private Action(String name) { this.name = name; }
+        public String getName() { return name; }
+        public static Action get(String name) { return lookup.get(name); }
     }
 
 
+    /**
+     * helper fn that logs the err and then calls the err callback
+     */
     private void error(CallbackContext cbCtx, String message) {
         LOG.e(LOG_TAG, message);
         cbCtx.error(message);
@@ -58,10 +50,9 @@ public class MixpanelPlugin extends CordovaPlugin {
 
 
     @Override
-    public boolean execute(String action, JSONArray args, final CallbackContext cbCtx){
+    public boolean execute(String action, JSONArray args, final CallbackContext cbCtx) {
         // throws JSONException
         Action act = Action.get(action);
-        JSONObject properties;
 
         if (act == null){
             this.error(cbCtx, "unknown action");
@@ -75,64 +66,19 @@ public class MixpanelPlugin extends CordovaPlugin {
 
         switch (act) {
             case FLUSH:
-                Runnable runnable = new Runnable() {
-                    @Override
-                    public void run() {
-                        mixpanel.flush();
-                        cbCtx.success();
-                    }
-                };
-                cordova.getThreadPool().execute(runnable);
-                cbCtx.success();
-                break;
-
+               return handleFlush(args, cbCtx);
             case IDENTIFY:
-                String uniqueId = args.optString(0, "");
-                if (TextUtils.isEmpty(uniqueId)) {
-                    this.error(cbCtx, "missing unique id");
-                    return false;
-                }
-                mixpanel.identify(uniqueId);
-                cbCtx.success();
-                break;
-
+                return handleIdentify(args, cbCtx);
             case INIT:
-                String token = args.optString(0, "");
-                if (TextUtils.isEmpty(token)) {
-                    this.error(cbCtx, "missing token for mixpanel project");
-                    return false;
-                }
-                Context ctx = cordova.getActivity();
-                mixpanel = MixpanelAPI.getInstance(ctx, token);
-                cbCtx.success();
-                break;
-
+                return handleInit(args, cbCtx);
             case RESET:
-                mixpanel.reset();
-                cbCtx.success();
-                break;
-
+                return handleReset(args, cbCtx);
             case TRACK:
-                String event = args.optString(0, "");
-                if (TextUtils.isEmpty(event)) {
-                    this.error(cbCtx, "missing event name");
-                    return false;
-                }
-                properties = args.optJSONObject(1);
-                if (properties == null) {
-                    properties = new JSONObject();
-                }
-                mixpanel.track(event, properties);
-                cbCtx.success();
-                break;
-
+                return handleTrack(args, cbCtx);
             default:
                 this.error(cbCtx, "unknown action");
                 return false;
         }
-
-        //success if got here
-        return true;
     }
 
 
@@ -143,4 +89,76 @@ public class MixpanelPlugin extends CordovaPlugin {
         }
         super.onDestroy();
     }
+
+    //
+    //  ACTION HANDLERS
+    //   - return true:
+    //     - to indicate action was executed with correct arguments
+    //     - also if the action from sdk has failed.
+    //  - return false:
+    //     - arguments were wrong
+
+
+    private boolean handleFlush(JSONArray args, final CallbackContext cbCtx) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                mixpanel.flush();
+                cbCtx.success();
+            }
+        };
+        cordova.getThreadPool().execute(runnable);
+        cbCtx.success();
+        return true;
+    }
+
+
+    private boolean handleIdentify(JSONArray args, final CallbackContext cbCtx) {
+        String uniqueId = args.optString(0, "");
+        if (TextUtils.isEmpty(uniqueId)) {
+            this.error(cbCtx, "missing unique id");
+            return false;
+        }
+        mixpanel.identify(uniqueId);
+        cbCtx.success();
+        return true;
+    }
+
+
+    private boolean handleInit(JSONArray args, final CallbackContext cbCtx) {
+        String token = args.optString(0, "");
+        if (TextUtils.isEmpty(token)) {
+            this.error(cbCtx, "missing token for mixpanel project");
+            return false;
+        }
+        Context ctx = cordova.getActivity();
+        mixpanel = MixpanelAPI.getInstance(ctx, token);
+        cbCtx.success();
+        return true;
+    }
+
+
+    private boolean handleReset(JSONArray args, final CallbackContext cbCtx) {
+        mixpanel.reset();
+        cbCtx.success();
+        return true;
+    }
+
+
+    private boolean handleTrack(JSONArray args, final CallbackContext cbCtx) {
+        String event = args.optString(0, "");
+        if (TextUtils.isEmpty(event)) {
+            this.error(cbCtx, "missing event name");
+            return false;
+        }
+
+        JSONObject properties = args.optJSONObject(1);
+        if (properties == null) {
+            properties = new JSONObject();
+        }
+        mixpanel.track(event, properties);
+        cbCtx.success();
+        return true;
+    }
+
 }
