ec6ecb91d7eaa6bd35fbf6953ee1f1767baaef8a
sam zilverberg <samzilverberg@gmail.com>
2015-05-29 00:46:25 +0200

    put code in JS to remember init status and check it before calling native code

--- www/mixpanel.js
+++ www/mixpanel.js
@@ -4,10 +4,12 @@ var exec = require('cordova/exec'),
   mixpanel = {
     people: {}
   },
+  initialized = false,
   errors = {
+    notInitialized: 'mixpanel not initialized',
     invalid: function(paramName, value) {
       return 'invalid ' + paramName + ': ' + value;
-    },
+    }
   };
 
 
@@ -15,30 +17,45 @@ var exec = require('cordova/exec'),
 
 
 mixpanel.alias = mixpanel.createAlias = function(alias, originalId, onSuccess, onFail) {
-  if (!alias) onFail(errors.invalid('alias', alias));
+  if (!initialized) onFail(errors.notInitialized);
+  else if (!alias) onFail(errors.invalid('alias', alias));
   else exec(onSuccess, onFail, 'Mixpanel', 'alias', [alias, originalId]);
 };
 
 mixpanel.flush = function(onSuccess, onFail) {
-  exec(onSuccess, onFail, 'Mixpanel', 'flush', []);
+  if (!initialized) onFail(errors.notInitialized);
+  else exec(onSuccess, onFail, 'Mixpanel', 'flush', []);
 };
 
 mixpanel.identify = function(id, onSuccess, onFail) {
-  if (!id) onFail(errors.invalid('id', id));
+  if (!initialized) onFail(errors.notInitialized);
+  else if (!id) onFail(errors.invalid('id', id));
   else exec(onSuccess, onFail, 'Mixpanel', 'identify', [id]);
 };
 
 mixpanel.init = function(token, onSuccess, onFail) {
   if (!token) onFail(errors.invalid('token', token));
-  else exec(onSuccess, onFail, 'Mixpanel', 'init', [token]);
+  else {
+    var wrappedSuccessFn = function() {
+        initialized = true;
+        onSuccess();
+      },
+      wrappedFailFn = function(err) {
+        initialized = false;
+        onFail(err);
+      };
+    exec(wrappedSuccessFn, wrappedFailFn, 'Mixpanel', 'init', [token]);
+  }
 };
 
 mixpanel.reset = function(onSuccess, onFail) {
-  exec(onSuccess, onFail, 'Mixpanel', 'reset', []);
+  if (!initialized) onFail(errors.notInitialized);
+  else exec(onSuccess, onFail, 'Mixpanel', 'reset', []);
 };
 
 mixpanel.track = function(eventName, eventProperties, onSuccess, onFail) {
-  if (!eventName) onFail(errors.invalid('event', eventName));
+  if (!initialized) onFail(errors.notInitialized);
+  else if (!eventName) onFail(errors.invalid('event', eventName));
   else exec(onSuccess, onFail, 'Mixpanel', 'track', [eventName, eventProperties]);
 };
 
@@ -47,12 +64,14 @@ mixpanel.track = function(eventName, eventProperties, onSuccess, onFail) {
 
 
 mixpanel.people.identify = function(distinctId, onSuccess, onFail) {
-  if (!distinctId) onFail(errors.invalid('distinctId', distinctId));
+  if (!initialized) onFail(errors.notInitialized);
+  else if (!distinctId) onFail(errors.invalid('distinctId', distinctId));
   else exec(onSuccess, onFail, 'Mixpanel', 'people_identify', [distinctId]);
 };
 
 mixpanel.people.set = function(peopleProperties, onSuccess, onFail) {
-  if (!peopleProperties || (typeof peopleProperties === 'object' && Object.keys(peopleProperties).length === 0)) onFail(errors.invalid('properties', peopleProperties));
+  if (!initialized) onFail(errors.notInitialized);
+  else if (!peopleProperties || (typeof peopleProperties === 'object' && Object.keys(peopleProperties).length === 0)) onFail(errors.invalid('properties', peopleProperties));
   else exec(onSuccess, onFail, 'Mixpanel', 'people_set', [peopleProperties]);
 };
 
